3679da65f76c8e9a0265b654f30b553d841c798c
maaaaaaaaad <integral5137@gmail.com>
2022-06-01 11:44:56 +0900

    config orm db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -20,11 +21,34 @@ import { ThrottlerModule } from '@nestjs/throttler';
         PORT: Joi.string().required(),
         TTL: Joi.number().required(),
         LIMIT: Joi.number().required(),
+        TYPEORM_NAME: Joi.string().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_DB_NAME: Joi.string().required(),
       }),
     }),
     ThrottlerModule.forRoot({
       ttl: +process.env.TTL,
       limit: +process.env.LIMIT,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
+        name: configService.get<string>('TYPEORM_NAME'),
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [],
