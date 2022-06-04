099b9933a63f8c5f241be5206a5fceaa4482a205
maaaaaaaaad <integral5137@gmail.com>
2022-06-04 13:58:58 +0900

    update users collection

--- src/auth/schemas/users.schema.ts
+++ src/auth/schemas/users.schema.ts
@@ -1,14 +1,30 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
+import * as bcrypt from 'bcrypt';
+import { NextFunction } from 'express';
+
+interface HashPassword extends Document {
+  password: string;
+}
 
 @Schema({ timestamps: true })
 export class Users extends Document {
-  @Prop({ required: [true, 'User email required'], unique: true, index: true })
+  @Prop({
+    required: [true, 'User email required'],
+    unique: true,
+    index: true,
+  })
   @IsEmail()
   email: string;
 
-  @Prop({ required: [true, 'User password required'] })
+  @Prop({
+    required: [true, 'User password required'],
+    validate: {
+      validator: (v: string): boolean => /(?=.*\d)(?=.*[a-z]).{8,}/.test(v),
+      message: 'Password is a combination of 8 or more numbers and letters.',
+    },
+  })
   @Matches(/(?=.*\d)(?=.*[a-z]).{8,}/, {
     message: 'Password is a combination of 8 or more numbers and letters.',
   })
@@ -16,4 +32,11 @@ export class Users extends Document {
   password: string;
 }
 
-export const UsersSchema = SchemaFactory.createForClass(Users);
+export const UsersSchema = SchemaFactory.createForClass(
+  Users,
+).pre<HashPassword>('save', async function (next: NextFunction) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
