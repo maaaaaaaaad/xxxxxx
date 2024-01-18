6d851bf658aa0e77112fad22c5a9daae1a650abf
maaaaaaaaad <integral5137@gmail.com>
2024-01-18 10:36:19 +0900

    e

--- output.c
+++ output.c
@@ -1,61 +1,25 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-
-using namespace std;
-
-#define ALP_MAX 150
-#define COP_MAX 150
-#define COST_MAX 1e9
-
-int mincost[ALP_MAX+1][COP_MAX+1];
-vector<vector<int> > arrP;
-int alp_max, cop_max;
-
-int getmincost(int alp, int cop) {
-	if(alp >= alp_max && cop >= cop_max){
-		return 0;
-	}
-
-	alp = min(alp, alp_max);
-    cop = min(cop, cop_max);
-
-	int &result = mincost[alp][cop];
-    if(result != -1)
-    	return result;
-    
-    result = COST_MAX;
-
-    if(alp < alp_max)
-    	result = min(result, getmincost(alp+1, cop) + 1);
-    if(cop < cop_max)
-    	result = min(result, getmincost(alp, cop+1) + 1);
-
-    for (auto &p : arrP) {
-        if (alp < p[0] || cop < p[1]) {
-            continue;
-        }
-        result = min(result, getmincost(alp+p[2], cop+p[3]) + p[4]);
-    }
-
-    return result;
-}
-
-int solution(int alp, int cop, vector<vector<int>> problems) {
-    arrP = problems;
-
-    alp_max = alp;
-    cop_max = cop;
-
-    for (auto &p : problems) {
-        alp_max = max(alp_max, p[0]);
-        cop_max = max(cop_max, p[1]);
-    }
-    
-    memset(mincost,-1,sizeof(mincost));
-    mincost[alp_max][cop_max] = 0;
-
-    return getmincost(alp, cop);
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int solution(vector<int> A, vector<int> B) 
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int answer = 0;
+    int A_Idx = A.size() - 1;
+    int B_Idx = B.size() - 1;
+    while (A_Idx >= 0)
+    {
+        int A_Score = A[A_Idx];
+        int B_Score = B[B_Idx];
+        if (A_Score < B_Score)
+        {
+            B_Idx--;
+            answer++;
+        }
+        A_Idx--;
+    }
+    return answer;
 }
