67c27a8fb9538a33f9e41ecff79713afa0fb4c21
maaaaaaaaad <integral5137@gmail.com>
2024-01-18 10:37:15 +0900

    e

--- output.c
+++ output.c
@@ -1,25 +1,47 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
- 
-int solution(vector<int> A, vector<int> B) 
-{
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
-    int answer = 0;
-    int A_Idx = A.size() - 1;
-    int B_Idx = B.size() - 1;
-    while (A_Idx >= 0)
-    {
-        int A_Score = A[A_Idx];
-        int B_Score = B[B_Idx];
-        if (A_Score < B_Score)
-        {
-            B_Idx--;
-            answer++;
-        }
-        A_Idx--;
-    }
-    return answer;
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#define MAX_DIST 1e9
+
+using namespace std;
+
+vector<int> rocksVec;
+
+int binarySearch(int distance, int n){
+    int left = 0;
+    int right = distance;
+    
+    int result = 0;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        
+        int cnt = 0;
+        int pos = 0;
+        int minDist = MAX_DIST;
+        for(int rock: rocksVec){
+            if(pos + mid <= rock){
+                cnt++;
+                minDist = min(minDist, rock - pos);
+                pos = rock;
+            }
+        }
+        
+        if(cnt >= rocksVec.size() - n){
+            left = mid+1;
+            result = max(result,minDist);
+        }else{
+            right = mid-1;
+        }
+    }
+    return result;
+}
+
+int solution(int distance, vector<int> rocks, int n) {
+    
+    rocksVec = rocks;
+    rocksVec.push_back(distance);
+    sort(rocksVec.begin(),rocksVec.end());
+    
+    return binarySearch(distance,n);
 }
